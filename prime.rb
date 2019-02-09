51197a8d7ce1730f0a2d7be5aeb34352845d8c31
Kat B <katreesebarnes@gmail.com>
2019-02-09 00:20:23 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,10 @@
 def prime?(num) 
   return false if !num.integer?
-  return false if num < 2 
-  return true if num == 0 
+  
+  return false if num < 2
+  
+  return true if num == 0
+  
   (2..num -1).each {|int| return false if num <= 1 || num % int == 0}
   
    true
